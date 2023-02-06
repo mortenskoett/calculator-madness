a1419eeabe574bc5b12762377831c506f2bb1449
Morten Skøtt <mortenskoett@gmail.com>
2023-02-06 22:51:58 +0100

    Finish impl server endpoint using grpc

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # calculator
 Microservice based equation calculator
+
+### Example
+Make a query against the running server using grpcurl
+```
+grpcurl -plaintext -d '{"equation":"1+1"}' localhost:8000 calculator.CalculationService/Run
+```
+
+List all grpc end points using grpcurl
+```
+grpcurl --plaintext localhost:8000 list
+```
--- src/calculator/cmd/server/main.go
+++ src/calculator/cmd/server/main.go
@@ -5,6 +5,11 @@ import (
 	"calculator/pkg/calc"
 	"context"
 	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 // calculationServer implements CalculationServiceServer interface
@@ -24,5 +29,21 @@ func (s *calculationServer) Run(context context.Context, calculationRequest *pb.
 }
 
 func main() {
-	fmt.Println("vim-go")
+	log.Println("Starting calculator GRPC protobuf service")
+	listener, err := net.Listen("tcp", ":8000")
+	if err != nil {
+		log.Fatalf("failed to create listener: %v", err)
+	}
+
+	server := grpc.NewServer()
+	pb.RegisterCalculationServiceServer(server, &calculationServer{})
+
+	// setup reflection to be able to work with grpcurl
+	reflection.Register(server)
+
+	log.Println("Serving at :8000")
+
+	if err := server.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
