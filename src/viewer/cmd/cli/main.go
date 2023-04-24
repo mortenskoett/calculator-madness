5e10996e2233d88a394197816effb22312bd5ba8
Morten Skøtt <mortenskoett@gmail.com>
2023-04-24 19:15:49 +0200

    Make env helper func sharable in viewer

--- src/viewer/cmd/cli/main.go
+++ src/viewer/cmd/cli/main.go
@@ -3,16 +3,16 @@ package main
 import (
 	"flag"
 	"log"
-	"os"
 	"shared/queue"
+	"viewer/pkg/env"
 )
 
 const (
-	ServiceNameChannel string = "viewer"
+	ServiceNameChannel string = "viewer-cli"
 )
 
 var (
-	nsqlookupdAddr = flag.String("nsqlookupd-addr", getEnvVarOrDefault("NSQLOOKUPD_ADDR", "127.0.0.1:4161"), "Address of nsqlookupd server with port")
+	nsqlookupdAddr = flag.String("nsqlookupd-addr", env.GetEnvVarOrDefault("NSQLOOKUPD_ADDR", "127.0.0.1:4161"), "Address of nsqlookupd server with port")
 )
 
 func calcStartedHandler(msg *queue.CalcStartedMessage, err error) error {
@@ -68,12 +68,4 @@ func main() {
 	consumer.AddCalcEndedHandler(calcEndedHandler)
 	consumer.Start()
 	defer consumer.Stop()
-}
-
-func getEnvVarOrDefault(envName string, def string) string {
-	envvar := os.Getenv(envName)
-	if len(envvar) == 0 {
-		return def
-	}
-	return envvar
 }
